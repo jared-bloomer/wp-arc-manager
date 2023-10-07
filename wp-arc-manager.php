3997550904e567293ccf10c79bd4965b5cbd8c56
Jared-Bloomer <me@jaredbloomer.com>
2023-10-07 19:28:03 -0400

    initial commit

---
+++ wp-arc-manager.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Plugin Name:       wp-arc-manager
+ * Description:       Wordpress Plugin for Managing an Amateur Radio Club.
+ * Version:           1.0.0
+ * Author:            KW4JLB
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       arc-manager
+ */
