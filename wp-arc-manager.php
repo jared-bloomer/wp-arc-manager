7b31391e682dee5d393f233b3e69b02f19a00ca8
Jared-Bloomer <me@jaredbloomer.com>
2023-10-07 19:58:35 -0400

    updating Text Domain

--- wp-arc-manager.php
+++ wp-arc-manager.php
@@ -11,7 +11,7 @@
  * Author URI:        https://jared-bloomer.github.io/KW4JLB-Documentation/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       arc-manager
+ * Text Domain:       wp-arc-manager
  */
 
  /*
